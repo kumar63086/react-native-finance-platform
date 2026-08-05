26e1e6080b78a053a6c2e095988e0d1377b63c18
kumar <128176566+kumar63086@users.noreply.github.com>
2026-08-05 08:30:18 +0530

    feat: implement auth routing, sign-in/up screens, and Zod validation with react-hook-form

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function SignIn() {
+export default function SignInScreen() {
     return (
       
     <View>
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function SignUp() {
+export default function SignUpScreen() {
   return (
     <View>
       <Text>Sign Up</Text>
