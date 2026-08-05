4b9ac8e39b125361aea3239cd69f60ee4c38fad5
kumar <128176566+kumar63086@users.noreply.github.com>
2026-08-05 11:23:07 +0530

    feat: implement sign-in and sign-up flows with email verification and add project assets

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,6 +1,33 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { Text, View } from "react-native";
+import { codeSchema, SignInFormValues, signInSchema } from "../lib/schemas/auth";
+import { useRouter } from "expo-router";
+import { useSignIn } from "@clerk/expo";
 
 export default function SignInScreen() {
+    const { signIn, errors, fetchStatus } = useSignIn();
+  const router = useRouter();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors: formErrors },
+  } = useForm<SignInFormValues>({
+    resolver: zodResolver(signInSchema),
+    mode: "onBlur",
+    defaultValues: { email: "", password: "" },
+  });
+
+  const {
+    control: codeControl,
+    handleSubmit: handleCodeSubmit,
+    formState: { errors: codeErrors },
+  } = useForm<{ code: string }>({
+    resolver: zodResolver(codeSchema),
+    mode: "onBlur",
+    defaultValues: { code: "" },
+  });
     return (
       
     <View>
--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,10 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Alert } from 'react-native'
 import React from 'react'
+import { useClerk, useUser } from '@clerk/expo';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { router } from 'expo-router';
 
 export default function ProfileScreen() {
+     const { user } = useUser();
+    const { signOut } = useClerk();
+    
+    const handleSignOut = () => {
+    Alert.alert("Sign out", "Are you sure you want to sign out?", [
+      { text: "Cancel", style: "cancel" },
+      {
+        text: "Sign out",
+        style: "destructive",
+        onPress: async () => {
+          await signOut();
+          router.replace("/sign-in");
+        },
+      },
+    ]);
+  };
   return (
-    <View>
-      <Text>profileScreen</Text>
-    </View>
+    <SafeAreaView>
+          <View>
+              <Text>Hello, {user?.firstName ?? 'friend'}</Text>
+              <TouchableOpacity
+                  onPress={handleSignOut }
+              >
+                  <Text >SignOut</Text>
+              </TouchableOpacity>
+              
+          </View>
+          
+    </SafeAreaView>
   )
 }
